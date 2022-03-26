5c4b4547efaf4528f23e8d9ee2e018c024aee70e
yanuarfrd <yanuarfrds@365.telkomuniversity.ac.id>
2022-03-26 12:09:23 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- MODUL5_1302204001/Program.cs
+++ MODUL5_1302204001/Program.cs
@@ -1,2 +1,88 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class SayaTubeVideo
+{
+    int id;
+    string title;
+    int playCount;
+
+
+    public SayaTubeVideo()
+    {
+        Random generate = new Random();
+        this.id = generate.Next(0, 100000);
+        this.playCount = 0;
+    }
+
+    public void IncreaseplayCount(int angka)
+    {
+        playCount = playCount + angka;
+    }
+
+    public int GetPlaycount()
+    {
+        return playCount;
+    }
+
+    public string GetTitle()
+    {
+        return title;
+    }
+
+    public void printVideoDetails()
+    {
+        Console.WriteLine("title: " + title);
+        Console.WriteLine("id: " + id);
+        Console.WriteLine("playCount:" + playCount);
+    }
+
+
+}
+
+
+public class SayaTubeUser
+{
+    int id;
+    List<SayaTubeVideo> uploadedVideo;
+    string Username;
+
+    public SayaTubeUser(string profile)
+    {
+        this.Username = profile;
+        Random generate = new Random();
+        this.id = generate.Next(0, 100000);
+        uploadedVideo = new List<SayaTubeVideo>();
+
+    }
+
+    public void GetTotalVideoplayCount()
+    {
+        int dataplayCount = 0;
+        foreach (SayaTubeVideo video in uploadedVideo)
+        {
+            dataplayCount = dataplayCount + video.GetPlaycount();
+        }
+    }
+
+    public void addVideo(SayaTubeVideo data)
+    {
+        uploadedVideo.Add(data);
+    }
+
+
+    public void printAllvideoplayCount()
+    {
+
+        for (int i = 0; i < uploadedVideo.Count; i++)
+        {
+            Console.WriteLine("user:" + Username);
+            Console.WriteLine("Video 1 judul:" + (i + 1) + ":" + uploadedVideo[i]);
+        }
+
+    }
+
+    Debug.Assert(username <= int.MaxValue && username >= int.MinValue);
+        {
+
+        }
+         
+
+}
